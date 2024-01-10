e685386d81869163434ec3f93f1ce9bc375824fa
Rustam Madaminov <rustam.madaminov@olc.uz>
2024-01-10 08:11:15 +0500

    kurslar

---
+++ courses/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Speciality, Teacher
+
+admin.site.register(Speciality)
+admin.site.register(Teacher)
---
+++ courses/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Speciality(models.Model):
+    name = models.CharField(max_length=28)
+    code = models.CharField(max_length=10, unique=True)
+    start_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "yo’nalishlar"
+        verbose_name = "yo’nalish"
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=True)
+    degree = models.CharField(max_length=10, blank=True)
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.first_name}-{self.speciality}'
+
+    class Meta:
+        verbose_name_plural = "o’qituvchilar"
+        verbose_name = "o’qituvchi"
+
+
